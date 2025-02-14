f265dfdc3c60fe01e8f5cf049292354e7cf81841
Zxmw <624994322@qq.com>
2025-02-14 08:50:02 +0000

    fix

--- src/processors/data_enricher_modules/image_search.py
+++ src/processors/data_enricher_modules/image_search.py
@@ -68,6 +68,7 @@ class BaiduImageCrawler:
                 opener = urllib.request.build_opener()
                 opener.addheaders = [
                     ('User-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'),
+                    ('referer', 'https://image.baidu.com/')
                 ]
                 urllib.request.install_opener(opener)
                 # 保存图片
