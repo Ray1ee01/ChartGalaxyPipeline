f71b5177343e37c8a3ea97949d60e94f7211ebf9
Zhen Li <thu.lz@outlook.com>
2025-05-03 10:22:46 +0000

    update tick label

--- framework/modules/chart_engine/template/d3-js/line_graph/bump_chart_02.js
+++ framework/modules/chart_engine/template/d3-js/line_graph/bump_chart_02.js
@@ -191,7 +191,7 @@ function makeChart(containerSelector, data) {
             .attr("text-anchor", "end")
             .attr("font-weight", "bold")
             .attr("font-size", 18)
-            .attr("fill", "#000")
+            .attr("fill", "#fff")
             .text(group);
     });
     // 右侧
@@ -203,50 +203,9 @@ function makeChart(containerSelector, data) {
             .attr("text-anchor", "start")
             .attr("font-weight", "bold")
             .attr("font-size", 18)
-            .attr("fill", "#000")
+            .attr("fill", "#fff")
             .text(group);
     });
-
-    // 6. 绘制x轴刻度（不显示domain和tick）
-    const xAxis = d3.axisBottom(xScale)
-        .tickValues(xTicks)
-        .tickFormat(xFormat)
-        .tickSize(0)  // 不显示tick
-        .tickPadding(10);
-    // 检查文本是否重叠
-    const xAxisG = g.append("g")
-        .attr("transform", `translate(0,${innerHeight})`)
-        .call(xAxis)
-        .call(g => g.select(".domain").remove());  // 不显示domain线
-    
-    // 获取所有刻度文本
-    const tickTexts = xAxisG.selectAll(".tick text");
-    
-    // 检查文本是否重叠
-    let overlap = false;
-    const textRects = [];
-    
-    tickTexts.each(function(d, i) {
-        const bbox = this.getBBox();
-        // 检查与前一个文本是否重叠
-        if (i > 0 && bbox.x < textRects[i-1].x + textRects[i-1].width + 5) {
-            overlap = true;
-        }
-        textRects.push({x: bbox.x, width: bbox.width});
-    });
-    
-    // 如果有重叠，旋转文本
-    if (overlap) {
-        tickTexts
-            .attr("transform", "rotate(-45)")
-            .attr("text-anchor", "end")
-            .attr("font-size", 16)
-            .attr("fill", "#000");
-    } else {
-        tickTexts
-            .attr("font-size", 16)
-            .attr("fill", "#000");
-    }
 
     return svg.node();
 } 
--- framework/modules/chart_engine/template/d3-js/line_graph/bump_chart_04.js
+++ framework/modules/chart_engine/template/d3-js/line_graph/bump_chart_04.js
@@ -229,7 +229,6 @@ function makeChart(containerSelector, data) {
         }
     });
 
-<<<<<<< HEAD
     // 6. 添加顶部时间标签（优化以避免重叠）
     // 创建一个临时的SVG元素来测量文本宽度
     const tempSvg = d3.select("body").append("svg").attr("width", 0).attr("height", 0);
@@ -300,48 +299,6 @@ function makeChart(containerSelector, data) {
             .attr("fill", "#fff")
             .text(xFormat(tick));
     });
-=======
-    // 6. 绘制x轴刻度（不显示domain和tick）
-    const xAxis = d3.axisBottom(xScale)
-        .tickValues(xTicks)
-        .tickFormat(xFormat)
-        .tickSize(0)  // 不显示tick
-        .tickPadding(10);
-    // 检查文本是否重叠
-    const xAxisG = g.append("g")
-        .attr("transform", `translate(0,${innerHeight})`)
-        .call(xAxis)
-        .call(g => g.select(".domain").remove());  // 不显示domain线
-    
-    // 获取所有刻度文本
-    const tickTexts = xAxisG.selectAll(".tick text");
-    
-    // 检查文本是否重叠
-    let overlap = false;
-    const textRects = [];
-    
-    tickTexts.each(function(d, i) {
-        const bbox = this.getBBox();
-        // 检查与前一个文本是否重叠
-        if (i > 0 && bbox.x < textRects[i-1].x + textRects[i-1].width + 5) {
-            overlap = true;
-        }
-        textRects.push({x: bbox.x, width: bbox.width});
-    });
-    
-    // 如果有重叠，旋转文本
-    if (overlap) {
-        tickTexts
-            .attr("transform", "rotate(-45)")
-            .attr("text-anchor", "end")
-            .attr("font-size", 16)
-            .attr("fill", "#000");
-    } else {
-        tickTexts
-            .attr("font-size", 16)
-            .attr("fill", "#000");
-    }
->>>>>>> origin/develop
 
     return svg.node();
 } 
